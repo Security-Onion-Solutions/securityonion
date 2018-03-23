7109598748859cd229f316f7dc06a5f538afefc6
Mike Reeves <mike.reeves@securityonionsolutions.com>
2018-03-23 15:07:56 -0400

    Setup Script - added fallback menu

--- so-setup-network.sh
+++ so-setup-network.sh
@@ -308,12 +308,27 @@ whiptail_install_type() {
   "MASTERONLY" "Start a new grid" OFF \
   "STORAGENODE" "Add a Storage Node" OFF 3>&1 1>&2 2>&3 )
 
+  local exitstatus=$?
+  if [[ "$exitstatus" = 1 ]]; then
+    whiptail_menu
+  fi
 }
 
 whiptail_management_nic() {
 
   MNIC=$(whiptail --title "NIC Setup" --radiolist "Please select your management NIC" 20 78 12 ${NICS[@]} 3>&1 1>&2 2>&3 )
 
+}
+
+whiptail_menu() {
+  MENU=$(whiptail --title "Menu example" --menu "Choose an option" 25 78 16 \
+"<-- Back" "Return to the main menu." \
+"Add User" "Add a user to the system." \
+"Modify User" "Modify an existing user." \
+"List Users" "List all users on the system." \
+"Add Group" "Add a user group to the system." \
+"Modify Group" "Modify a group and its list of members." \
+"List Groups" "List all groups on the system.")
 }
 
 whiptail_nids() {
