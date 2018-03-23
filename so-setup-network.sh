225dfe3b9c6c92254900c81952e5070a195d9e75
Mike Reeves <mike.reeves@securityonionsolutions.com>
2018-03-23 15:16:28 -0400

    Setup Script - added fallback menu

--- so-setup-network.sh
+++ so-setup-network.sh
@@ -329,7 +329,7 @@ whiptail_menu() {
 "List Users" "List all users on the system." \
 "Add Group" "Add a user group to the system." \
 "Modify Group" "Modify a group and its list of members." \
-"List Groups" "List all groups on the system.")
+"List Groups" "List all groups on the system." 3>&2 2>&1 1>&3)
 }
 
 whiptail_nids() {
