99294dbdb2887155dea05fd36cf4eb36bc756e36
Mike Reeves <mike.reeves@securityonionsolutions.com>
2018-03-27 10:04:31 -0400

    Setup Script - Fix Path for ssh key

--- so-setup-network.sh
+++ so-setup-network.sh
@@ -112,9 +112,10 @@ configure_sensor_pillar() {
 
 copy_ssh_key() {
   # Generate and copy SSH key
-  cat /dev/zero | ssh-keygen -f /root/.ssh/so.key -t rsa -q -N ""
+  cat /dev/zero | ssh-keygen -f ~/.ssh/so.key -t rsa -q -N ""
+  chown $SUDO_USER:$SUDO_USER ~/.ssh/so.key*
   #Copy the key over to the master
-  sudo ssh-copy-id -i /root/.ssh/so.key socore@$MASTER
+  sudo ssh-copy-id -i ~/.ssh/so.key socore@$MASTER
 }
 
 create_bond() {
