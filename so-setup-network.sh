a9bc48b4bd3fd9cf0ac845b59824dae239afe69e
Mike Reeves <mike.reeves@securityonionsolutions.com>
2018-03-27 15:49:12 -0400

    Setup Script - Salt Key rework

--- so-setup-network.sh
+++ so-setup-network.sh
@@ -43,7 +43,7 @@ accept_salt_key_local() {
 
 accept_salt_key_remote() {
   # Accept the key
-  SSHCOMMANDS="sudo salt-key -ya $HOSTNAME"
+  SSHCOMMANDS='sudo salt-key -ya $HOSTNAME'
   ssh -i ~/.ssh/so.key socore@$MSRV < $SSHCOMMANDS
 
 }
