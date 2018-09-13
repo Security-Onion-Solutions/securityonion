0079d4b958c877663c7de44a6a3a0d3cfaf47be0
Mike Reeves <luke@geekempire.com>
2018-09-12 21:24:48 -0400

    SO Setup - Too many FIz

--- so-setup-network.sh
+++ so-setup-network.sh
@@ -411,7 +411,9 @@ saltify() {
 
     if [ $INSTALLTYPE == 'MASTERONLY' ] || [ $INSTALLTYPE == 'EVALMODE' ]; then
       yum -y install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
+
     else
+
       if [ $MASTERUPDATES == 'MASTER' ]; then
         yum -y install wget
         export http_proxy=http://$MSRV:3142; wget -O $TMP/salt-repo-latest-2.el7.noarch.rpm http://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
@@ -420,8 +422,7 @@ saltify() {
         yum -y install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
       fi
     fi
-  fi
-
+  
     yum clean expire-cache
     yum -y install salt-minion yum-utils device-mapper-persistent-data lvm2 openssl
     yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
