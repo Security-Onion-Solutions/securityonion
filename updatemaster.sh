40037f06d24b9f215b00cde13c967b8963ed96ba
Mike Reeves <luke@geekempire.com>
2018-10-02 11:11:16 -0400

    Update Script - Fix file perms

--- updatemaster.sh
+++ updatemaster.sh
@@ -7,3 +7,5 @@ git clone https://github.com/TOoSmOotH/securityonion-saltstack.git
 cd securityonion-saltstack
 rsync -a pillar /opt/so/saltstack/
 rsync -a --exclude-from 'exclude-list.txt' salt /opt/so/saltstack/
+chown -R socore:socore /opt/so
+chmod 755 /opt/so/saltstack/pillar/firewall/addfirewall.sh
