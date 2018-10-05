3349718383ce4d4bf695d543278ccd105a23a34c
Mike Reeves <luke@geekempire.com>
2018-10-05 18:28:30 -0400

    Update Script - Run highstate to fix perms

--- updatemaster.sh
+++ updatemaster.sh
@@ -9,5 +9,6 @@ rsync -a pillar /opt/so/saltstack/
 rsync -a --exclude-from 'exclude-list.txt' salt /opt/so/saltstack/
 chown -R socore:socore /opt/so
 chmod 755 /opt/so/saltstack/pillar/firewall/addfirewall.sh
+cd ~
 rm -rf /tmp/sogh
 salt-call state.highstate
