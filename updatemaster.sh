c935191d41cf8f37468bac95b11f4eabddc66e77
Mike Reeves <luke@geekempire.com>
2018-11-12 12:33:03 -0500

    Update Script - Only sync the salt dir

--- updatemaster.sh
+++ updatemaster.sh
@@ -5,9 +5,8 @@ mkdir /tmp/sogh
 cd /tmp/sogh
 git clone https://github.com/TOoSmOotH/securityonion-saltstack.git
 cd securityonion-saltstack
-rsync -a pillar /opt/so/saltstack/
 rsync -a --exclude-from 'exclude-list.txt' salt /opt/so/saltstack/
-chown -R socore:socore /opt/so
+chown -R socore:socore /opt/so/saltstack/salt
 chmod 755 /opt/so/saltstack/pillar/firewall/addfirewall.sh
 cd ~
 rm -rf /tmp/sogh
