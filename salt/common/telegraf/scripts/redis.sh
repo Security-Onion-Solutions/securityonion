398503e292ebc5da11345a6a79f611b4ce4e9546
Mike Reeves <luke@geekempire.com>
2018-11-20 14:58:05 -0500

    Common Module - Fix redis script

--- salt/common/telegraf/scripts/redis.sh
+++ salt/common/telegraf/scripts/redis.sh
@@ -3,4 +3,4 @@
 UNPARSED=$(redis-cli llen logstash:unparsed | awk '{print $1}')
 PARSED=$(redis-cli llen logstash:parsed | awk '{print $1}')
 
-echo "redisqueue unparsed=$UNPARSED parsed=$PARSED"
+echo "redisqueue unparsed=$UNPARSED,parsed=$PARSED"
