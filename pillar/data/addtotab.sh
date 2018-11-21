257d87684f0d256a9174bd57cc6d84095c78411c
Mike Reeves <luke@geekempire.com>
2018-11-21 09:47:27 -0500

    Change the sensortab to add additional informations

--- pillar/data/addtotab.sh
+++ pillar/data/addtotab.sh
@@ -5,12 +5,18 @@
 TYPE=$1
 NAME=$2
 IPADDRESS=$3
+MANINT=$4
+MONINT=$5
 
 if grep -q $IPADDRESS "/opt/so/saltstack/pillar/data/$1.sls"; then
   echo "Node Already Present"
 else
   echo "  $NAME:" >> /opt/so/saltstack/pillar/data/$1.sls
   echo "    ip: $IPADDRESS" >> /opt/so/saltstack/pillar/data/$1.sls
+  if [ $TYPE == 'sensorstab' ]; then
+    echo " manint: $MANINT" >> /opt/so/saltstack/pillar/data/$1.sls
+    echo " monint: $MONINT" >> /opt/so/saltstack/pillar/data/$1.sls
+  fi
   salt-call state.apply utility
 
 fi
