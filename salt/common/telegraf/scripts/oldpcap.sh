7dd5755120dff4088f9cfe61de37a5d05364bd42
Mike Reeves <luke@geekempire.com>
2018-11-21 20:13:35 -0500

    Telegraf - Track Oldest PCAP

--- salt/common/telegraf/scripts/oldpcap.sh
+++ salt/common/telegraf/scripts/oldpcap.sh
@@ -3,6 +3,6 @@
 # Get the data
 OLDPCAP=$(find /nsm/pcap -type f -printf '%Cs %p\n' | sort | head -n 1 | awk {'print $1'})
 DATE=$(date +%s)
-AGE=$(expr $DATE - $OLDPCAP)
+AGE=$(($DATE - $OLDPCAP))
 
 echo "pcapage seconds=$AGE"
