7b3f9459ee8a7093dcf36b5581c3f87d5df952c5
Mike Reeves <luke@geekempire.com>
2018-11-26 19:17:46 -0500

    Common Module - Add Master Tab

--- pillar/data/addtotab.sh
+++ pillar/data/addtotab.sh
@@ -6,7 +6,7 @@ TYPE=$1
 NAME=$2
 IPADDRESS=$3
 CPUS=$4
-UID=$5
+GUID=$5
 MANINT=$6
 MONINT=$7
 
@@ -18,7 +18,7 @@ else
   echo "    ip: $IPADDRESS" >> /opt/so/saltstack/pillar/data/$1.sls
   echo "    manint: $MANINT" >> /opt/so/saltstack/pillar/data/$1.sls
   echo "    totalcpus: $CPUS" >> /opt/so/saltstack/pillar/data/$1.sls
-  echo "    guid: $UID" >> /opt/so/saltstack/pillar/data/$1.sls
+  echo "    guid: $GUID" >> /opt/so/saltstack/pillar/data/$1.sls
   if [ $TYPE == 'sensorstab' ]; then
     echo "    monint: $MONINT" >> /opt/so/saltstack/pillar/data/$1.sls
   fi
