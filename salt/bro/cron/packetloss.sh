a824d066559b83abc44509a5524b1a1ef63f3e3b
Mike Reeves <luke@geekempire.com>
2018-11-29 19:13:28 -0500

    Bro - Add cron for checking PL

---
+++ salt/bro/cron/packetloss.sh
@@ -0,0 +1,1 @@
+docker exec -it so-bro /opt/bro/bin/broctl netstats | awk -F '[ =]' '{RCVD += $5;DRP += $7;TTL += $9} END { print "rcvd: " RCVD, "dropped: " DRP, "total: " TTL}' >> /nsm/bro/logs/packetloss.log
