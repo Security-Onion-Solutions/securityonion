d9b8bc08c25dbe5cb5078a7f502d55b868896482
Josh Brower <josh@defensivedepth.com>
2019-04-03 12:28:58 -0400

    update so-fleet-setup.sh for new hh-launcher docker

--- salt/fleet/so-fleet-setup.sh
+++ salt/fleet/so-fleet-setup.sh
@@ -16,12 +16,12 @@ esecret=$(sudo docker exec so-fleet fleetctl get enroll-secret)
 cat /etc/pki/fleet.crt /etc/pki/ca.crt > /etc/pki/fleet-launcher.crt
 
 #Create the output directory
-mkdir /opt/so/osquery
+mkdir /opt/so/conf/fleet/packages
 
 docker run \
-  --mount type=bind,source=/opt/so/osquery,target=/output \
-  --mount type=bind,source=/etc/pki/fleet-launcher.crt,target=/var/launcher/fleet-launcher.crt \
-  defensivedepth/soq-launcher "$esecret" "$1"
+  --mount type=bind,source=/opt/so/conf/fleet/packages,target=/output \
+  --mount type=bind,source=/etc/pki/fleet-launcher.crt,target=/var/launcher/launcher.crt \
+  defensivedepth/hh-launcher "$esecret" "$1":8080
 
 echo "Fleet Setup Complete - Login here: https://$1"
 echo "Your username is $2 and your password is $initpw"
