73b90dd4ba790ec5e40943d607ec278f27ef65b4
Mike Reeves <luke@geekempire.com>
2019-04-22 19:29:23 -0400

    Fleet Module - Updated launcher docker

--- salt/fleet/so-fleet-setup.sh
+++ salt/fleet/so-fleet-setup.sh
@@ -2,12 +2,12 @@
 
 if [ ! "$(docker ps -q -f name=so-fleet)" ]; then
         echo "so-fleet container not running... Exiting..."
-        exit 1 
+        exit 1
 fi
 
 initpw=$(date +%s | sha256sum | base64 | head -c 16 ; echo)
 
-docker exec so-fleet fleetctl config set --address https://$1:443 --tls-skip-verify        
+docker exec so-fleet fleetctl config set --address https://$1:443 --tls-skip-verify
 docker exec so-fleet fleetctl setup --email $2 --password $initpw
 
 docker exec so-fleet fleetctl apply -f /packs/palantir/Fleet/Endpoints/options.yaml
@@ -26,7 +26,7 @@ mkdir /opt/so/conf/fleet/packages
 docker run \
   --mount type=bind,source=/opt/so/conf/fleet/packages,target=/output \
   --mount type=bind,source=/etc/pki/fleet-launcher.crt,target=/var/launcher/launcher.crt \
-  defensivedepth/hh-launcher "$esecret" "$1":8080
+  soshybridhunter/so-fleet-launcher:HH1.0.8 "$esecret" "$1":8080
 
 #Update timestamp on packages webpage
 sed -i "s@.*Generated.*@Generated: $(date '+%m%d%Y')@g" /opt/so/conf/fleet/packages/index.html
