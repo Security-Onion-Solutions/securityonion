8f2e3e87ff761d3f7e2c3e7ba14cb69f629beec5
Mike Reeves <mike.reeves@securityonionsolutions.com>
2019-09-24 14:18:54 -0400

    Setup Script - Actually check the passwords

--- so-setup-network.sh
+++ so-setup-network.sh
@@ -1626,6 +1626,7 @@ if (whiptail_you_sure); then
     whiptail_create_socore_user
     whiptail_create_socore_user_password1
     whiptail_create_socore_user_password2
+    check_admin_pass
 
     # Last Chance to back out
     whiptail_make_changes
@@ -1847,6 +1848,7 @@ if (whiptail_you_sure); then
     whiptail_create_socore_user
     whiptail_create_socore_user_password1
     whiptail_create_socore_user_password2
+    check_admin_pass
     whiptail_make_changes
     set_hostname
     generate_passwords
