1cc2365cf129500cc38e1fcc6b473b79af99d513
Mike Reeves <mike.reeves@securityonionsolutions.com>
2019-12-03 16:17:47 -0500

    New Setup Script - Fix Path

--- so-setup-network.sh
+++ so-setup-network.sh
@@ -15,4 +15,4 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-bash setup/so-setup.sh network
+cd setup && bash so-setup.sh network
