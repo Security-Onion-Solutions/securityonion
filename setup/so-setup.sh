0bb5922372e98990cff32970a1757b32dccbb357
Mike Reeves <mike.reeves@securityonionsolutions.com>
2019-12-09 10:26:25 -0500

    Fix Setup Syntax

--- setup/so-setup.sh
+++ setup/so-setup.sh
@@ -152,7 +152,7 @@ if (whiptail_you_sure) ; then
   ####################
   ##     Master     ##
   ####################
-  if [ $INSTALLTYPE == 'HELIXSESOR']; then
+  if [ $INSTALLTYPE == 'HELIXSESOR' ]; then
     whiptail_helix_apikey
     whiptail_homenet_master
     whiptail_rule_setup
