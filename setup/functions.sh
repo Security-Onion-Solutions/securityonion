ed6ca5a3a99c6b54344ba367238412fe29855be7
Mike Reeves <mike.reeves@securityonionsolutions.com>
2019-12-12 16:45:19 -0500

    Setup - copy from the onion home dir vs root

--- setup/functions.sh
+++ setup/functions.sh
@@ -1078,8 +1078,8 @@ salt_master_directories() {
 
   # Copy over the salt code and templates
   if [ $INSTALLMETHOD == 'iso' ]; then
-    cp -R /root/SecurityOnion/pillar/* /opt/so/saltstack/pillar/
-    cp -R /root/SecurityOnion/salt/* /opt/so/saltstack/salt/
+    cp -R /home/onion/SecurityOnion/pillar/* /opt/so/saltstack/pillar/
+    cp -R /home/onion/SecurityOnion/salt/* /opt/so/saltstack/salt/
   else
     cp -R $SCRIPTDIR/../pillar/* /opt/so/saltstack/pillar/
     cp -R $SCRIPTDIR/../salt/* /opt/so/saltstack/salt/
