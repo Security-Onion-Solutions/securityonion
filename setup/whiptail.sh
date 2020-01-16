593c8a9ea5f8357de96e5aba5b0ba13939de08a8
Mike Reeves <mike.reeves@securityonionsolutions.com>
2020-01-16 11:24:17 -0500

    If contains localhost don't allow to move forward

--- setup/whiptail.sh
+++ setup/whiptail.sh
@@ -672,7 +672,7 @@ whiptail_set_hostname() {
   local exitstatus=$?
   whiptail_check_exitstatus $exitstatus
 
-  while [[ "$HOSTNAME" == 'localhost' ]] ; do
+  while [[ "$HOSTNAME" == *'localhost'* ]] ; do
     whiptail --title "Security Onion Setup" --msgbox "Please choose a hostname that isn't localhost." 8 75
     HOSTNAME=$(whiptail --title "Security Onion Setup" --inputbox \
     "Enter the Hostname you would like to set." 10 75 $HOSTNAME 3>&1 1>&2 2>&3)
