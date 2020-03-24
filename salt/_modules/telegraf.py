7e6c70aff2dca7aea9124def3717529e31606639
m0duspwnens <josh.patterson@securityonionsolutions.com>
2020-03-24 17:27:55 -0400

    add modules to be used in monitoring - https://github.com/Security-Onion-Solutions/securityonion-saltstack/issues/90

---
+++ salt/_modules/telegraf.py
@@ -0,0 +1,16 @@
+#!py
+
+import logging
+import socket
+
+
+def send(data):
+
+  mainint = __salt__['pillar.get']('node:mainint')
+  mainip = __salt__['grains.get']('ip_interfaces').get(mainint)[0]
+  dstport = 8094
+
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sent = sock.sendto(data.encode('utf-8'), (mainip, dstport))
+
+  return sent
