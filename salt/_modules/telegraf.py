86c13fc3929ea44731a864f1a4237642649665d4
m0duspwnens <josh.patterson@securityonionsolutions.com>
2020-04-08 16:24:15 -0400

    fix salt telegraf module

--- salt/_modules/telegraf.py
+++ salt/_modules/telegraf.py
@@ -6,7 +6,7 @@ import socket
 
 def send(data):
 
-  mainint = __salt__['pillar.get']('node:mainint')
+  mainint = __salt__['pillar.get']('sensor:mainint', __salt__['pillar.get']('master:mainint'))
   mainip = __salt__['grains.get']('ip_interfaces').get(mainint)[0]
   dstport = 8094
 
