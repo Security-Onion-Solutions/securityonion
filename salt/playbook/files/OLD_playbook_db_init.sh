2a100c0dcc578369d10ad6dc1af0f1ce7eb7457a
William Wernert <william.wernert@securityonionsolutions.com>
2020-10-02 14:34:30 -0400

    Add OLD_ prefix + only update rules if playbook enabled

--- salt/playbook/files/OLD_playbook_db_init.sh
+++ salt/playbook/files/OLD_playbook_db_init.sh
@@ -4,5 +4,5 @@
 
 default_salt_dir=/opt/so/saltstack/default
 
-docker cp $default_salt_dir/salt/playbook/files/playbook_db_init-old.sql so-mysql:/tmp/playbook_db_init.sql
+docker cp $default_salt_dir/salt/playbook/files/OLD_playbook_db_init.sql so-mysql:/tmp/playbook_db_init.sql
 docker exec so-mysql /bin/bash -c "/usr/bin/mysql -b  -uroot -p{{MYSQLPASS}}  < /tmp/playbook_db_init.sql"
