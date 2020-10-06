48ca2cdff121b49399e78a7f4ed613ec1f86ad04
m0duspwnens <josh.patterson@securityonionsolutions.com>
2020-10-06 18:10:41 -0400

    fix pillars we check

--- salt/telegraf/scripts/zeekcaptureloss.sh
+++ salt/telegraf/scripts/zeekcaptureloss.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-{% set WORKERS = salt['pillar.get']('sensor:zeekprocs', salt['pillar.get']('sensor:zeekpins') | length) %}
+{% set WORKERS = salt['pillar.get']('sensor:zeek_lbprocs', salt['pillar.get']('sensor:zeek_pins') | length) %}
 ZEEKLOG=/host/nsm/zeek/logs/current/capture_loss.log
 if [ -f "$ZEEKLOG" ]; then
   LOSS=$(tail -{{WORKERS}} $ZEEKLOG | awk -F, '{print $NF}' | sed 's/}//' | awk -F: '{LOSS += $2 / {{WORKERS}}} END { print "loss: " LOSS}')
