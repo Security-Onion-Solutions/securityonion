af2df2c7d1da2fe38b45d85d914429488e6d5d82
m0duspwnens <josh.patterson@securityonionsolutions.com>
2020-10-06 18:44:22 -0400

    just print the loss

--- salt/telegraf/scripts/zeekcaptureloss.sh
+++ salt/telegraf/scripts/zeekcaptureloss.sh
@@ -2,6 +2,6 @@
 {% set WORKERS = salt['pillar.get']('sensor:zeek_lbprocs', salt['pillar.get']('sensor:zeek_pins') | length) %}
 ZEEKLOG=/host/nsm/zeek/spool/logger/capture_loss.log
 if [ -f "$ZEEKLOG" ]; then
-  LOSS=$(tail -{{WORKERS}} $ZEEKLOG | awk -F, '{print $NF}' | sed 's/}//' | awk -F: '{LOSS += $2 / {{WORKERS}}} END { print "loss: " LOSS}')
+  LOSS=$(tail -{{WORKERS}} $ZEEKLOG | awk -F, '{print $NF}' | sed 's/}//' | awk -F: '{LOSS += $2 / {{WORKERS}}} END { print LOSS}')
   echo "zeekcaptureloss loss=$LOSS"
 fi
