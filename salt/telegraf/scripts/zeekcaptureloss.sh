f95712c502d78d8921bb8dbd8921fab8ada29dbc
m0duspwnens <josh.patterson@securityonionsolutions.com>
2020-10-06 18:38:51 -0400

    update log file

--- salt/telegraf/scripts/zeekcaptureloss.sh
+++ salt/telegraf/scripts/zeekcaptureloss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 {% set WORKERS = salt['pillar.get']('sensor:zeek_lbprocs', salt['pillar.get']('sensor:zeek_pins') | length) %}
-ZEEKLOG=/host/nsm/zeek/logs/current/capture_loss.log
+ZEEKLOG=/host/nsm/zeek/spool/logger/capture_loss.log
 if [ -f "$ZEEKLOG" ]; then
   LOSS=$(tail -{{WORKERS}} $ZEEKLOG | awk -F, '{print $NF}' | sed 's/}//' | awk -F: '{LOSS += $2 / {{WORKERS}}} END { print "loss: " LOSS}')
   echo "zeekcaptureloss loss=$LOSS"
