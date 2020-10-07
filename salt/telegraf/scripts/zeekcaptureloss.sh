6f2d47cc40ba08508944f3625c4a4cdf6243ab5f
m0duspwnens <josh.patterson@securityonionsolutions.com>
2020-10-07 11:11:06 -0400

    only send loss if timestamp on data has changed

--- salt/telegraf/scripts/zeekcaptureloss.sh
+++ salt/telegraf/scripts/zeekcaptureloss.sh
@@ -2,11 +2,11 @@
 # This script returns the average of all the workers average capture loss to telegraf / influxdb in influx format include nanosecond precision timestamp
 {%- set WORKERS = salt['pillar.get']('sensor:zeek_lbprocs', salt['pillar.get']('sensor:zeek_pins') | length) %}
 ZEEKLOG=/host/nsm/zeek/spool/logger/capture_loss.log
-LASTCAPTURELOSSLOG=/script/lastcaptureloss.txt
+LASTCAPTURELOSSLOG=/scripts/lastcaptureloss.txt
 if [ -f "$ZEEKLOG" ]; then
   CURRENTTS=$(tail -1 $ZEEKLOG | jq .ts | sed 's/"//g')
   if [ -f "$LASTCAPTURELOSSLOG" ]; then
-    LASTTS=$(cat /script/lastcaptureloss.txt)
+    LASTTS=$(cat $LASTCAPTURELOSSLOG)
     if [[ "$LASTTS" != "$CURRENTTS" ]]; then
       LOSS=$(tail -{{WORKERS}} $ZEEKLOG | awk -F, '{print $NF}' | sed 's/}//' | awk -F: '{LOSS += $2 / {{WORKERS}}} END { print LOSS}')
       echo "zeekcaptureloss loss=$LOSS"
