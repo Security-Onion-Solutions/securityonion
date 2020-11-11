ea1227de9dde68378d0cf78c706520b3f2d0435f
Mike Reeves <luke@geekempire.com>
2020-11-11 11:16:15 -0500

    Update checkfiles.sh

--- salt/telegraf/scripts/checkfiles.sh
+++ salt/telegraf/scripts/checkfiles.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+APP=checkfiles
+lf=/tmp/$APP-pidLockFile
+# create empty lock file if none exists
+cat /dev/null >> $lf
+read lastPID < $lf
+# if lastPID is not null and a process with that pid exists , exit
+[ ! -z "$lastPID" -a -d /proc/$lastPID ] && exit
+echo $$ > $lf
+
 FILES=$(ls -1x /host/nsm/faf/complete/ | wc -l)
 
 echo "faffiles files=$FILES"
