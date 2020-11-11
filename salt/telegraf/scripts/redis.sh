d3f65ac1a8e431de74815d931d35d3ffce5b9268
Mike Reeves <luke@geekempire.com>
2020-11-11 11:22:52 -0500

    Update redis.sh

--- salt/telegraf/scripts/redis.sh
+++ salt/telegraf/scripts/redis.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+APP=redis
+lf=/tmp/$APP-pidLockFile
+# create empty lock file if none exists
+cat /dev/null >> $lf
+read lastPID < $lf
+# if lastPID is not null and a process with that pid exists , exit
+[ ! -z "$lastPID" -a -d /proc/$lastPID ] && exit
+echo $$ > $lf
+
 UNPARSED=$(redis-cli llen logstash:unparsed | awk '{print $1}')
 PARSED=$(redis-cli llen logstash:parsed | awk '{print $1}')
 
