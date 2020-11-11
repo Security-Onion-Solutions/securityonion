cb46c13054b17a6ddb46e7728a1a76099d46b077
Mike Reeves <luke@geekempire.com>
2020-11-11 11:22:28 -0500

    Update oldpcap.sh

--- salt/telegraf/scripts/oldpcap.sh
+++ salt/telegraf/scripts/oldpcap.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+APP=oldpcap
+lf=/tmp/$APP-pidLockFile
+# create empty lock file if none exists
+cat /dev/null >> $lf
+read lastPID < $lf
+# if lastPID is not null and a process with that pid exists , exit
+[ ! -z "$lastPID" -a -d /proc/$lastPID ] && exit
+echo $$ > $lf
+
 # Get the data
 OLDPCAP=$(find /host/nsm/pcap -type f -exec stat -c'%n %Z' {} + | sort | grep -v "\." | head -n 1 | awk {'print $2'})
 DATE=$(date +%s)
