edb0d71e87858c55091e56fb987be399ca0aafe6
Mike Reeves <luke@geekempire.com>
2020-11-11 11:43:28 -0500

    Update zeekloss.sh

--- salt/telegraf/scripts/zeekloss.sh
+++ salt/telegraf/scripts/zeekloss.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # This script returns the packets dropped by Zeek, but it isn't a percentage. $LOSS * 100 would be the percentage
+
+APP=zeekloss
+lf=/tmp/$APP-pidLockFile
+# create empty lock file if none exists
+cat /dev/null >> $lf
+read lastPID < $lf
+# if lastPID is not null and a process with that pid exists , exit
+[ ! -z "$lastPID" -a -d /proc/$lastPID ] && exit
+echo $$ > $lf
+
 ZEEKLOG=$(tac /host/nsm/zeek/logs/packetloss.log | head -2)
 declare RESULT=($ZEEKLOG)
 CURRENTDROP=${RESULT[3]}
