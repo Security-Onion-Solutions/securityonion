a4d3e109e6c9a50df0f54edb30ddd94c47e13a97
Mike Reeves <luke@geekempire.com>
2020-11-11 11:17:18 -0500

    Update influxdbsize.sh

--- salt/telegraf/scripts/influxdbsize.sh
+++ salt/telegraf/scripts/influxdbsize.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+APP=influxsize
+lf=/tmp/$APP-pidLockFile
+# create empty lock file if none exists
+cat /dev/null >> $lf
+read lastPID < $lf
+# if lastPID is not null and a process with that pid exists , exit
+[ ! -z "$lastPID" -a -d /proc/$lastPID ] && exit
+echo $$ > $lf
+
 INFLUXSIZE=$(du -s -k /host/nsm/influxdb | awk {'print $1'})
 
 echo "influxsize kbytes=$INFLUXSIZE"
