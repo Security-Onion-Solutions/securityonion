9548b3df54302bcf3ee2a46444ccde327335bbb2
Mike Reeves <luke@geekempire.com>
2020-11-11 11:23:20 -0500

    Update stenoloss.sh

--- salt/telegraf/scripts/stenoloss.sh
+++ salt/telegraf/scripts/stenoloss.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+APP=stenoloss
+lf=/tmp/$APP-pidLockFile
+# create empty lock file if none exists
+cat /dev/null >> $lf
+read lastPID < $lf
+# if lastPID is not null and a process with that pid exists , exit
+[ ! -z "$lastPID" -a -d /proc/$lastPID ] && exit
+echo $$ > $lf
+
 # Get the data
 DROP=$(tac /var/log/stenographer/stenographer.log | grep -m1 drop | awk '{print $14}' | awk -F "=" '{print $2}')
 
