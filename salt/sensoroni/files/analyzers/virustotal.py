0a8d24a22520af82fc988883cb8b721960caf05a
Jason Ertel <jason.ertel@securityonionsolutions.com>
2022-03-29 13:10:04 -0400

    Add automated CI for analyzers

--- salt/sensoroni/files/analyzers/virustotal.py
+++
@@ -1,5 +0,0 @@
-def main():
-	print '{"foo":"bar","summary":"something here"}'
-
-if __name__ == "__main__":
-	main()
--- salt/sensoroni/files/analyzers/whois/whois.py
+++ salt/sensoroni/files/analyzers/whois/whois.py
@@ -1,5 +1,5 @@
 def main():
-	print '{"result":{ "requestId": "something-generated-by-whois", "someother_field": "more data" }, "summary": "botsrv.btc-goblin.ru"}'
+	print('{"result":{ "requestId": "something-generated-by-whois", "someother_field": "more data" }, "summary": "botsrv.btc-goblin.ru"}')
 
 if __name__ == "__main__":
 	main()
