cb491630ae7b4393b38c1c2c539e02f044feaf85
Jason Ertel <jason.ertel@securityonionsolutions.com>
2022-03-29 13:40:56 -0400

    Analyzer CI

--- salt/sensoroni/files/analyzers/whois/whois.py
+++ salt/sensoroni/files/analyzers/whois/whois.py
@@ -1,5 +1,6 @@
 def main():
-	print('{"result":{ "requestId": "something-generated-by-whois", "someother_field": "more data" }, "summary": "botsrv.btc-goblin.ru"}')
+    print('{"result":{ "requestId": "something-generated-by-whois", "someother_field": "more data" }, "summary": "botsrv.btc-goblin.ru"}')
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- salt/sensoroni/files/analyzers/whois/whois_test.py
+++ salt/sensoroni/files/analyzers/whois/whois_test.py
@@ -3,10 +3,11 @@ from unittest.mock import patch
 from whois import whois
 import unittest
 
+
 class TestWhoisMethods(unittest.TestCase):
 
     def test_main(self):
-        with patch('sys.stdout', new = StringIO()) as mock_stdout:
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
             whois.main()
             expected = '{"result":{ "requestId": "something-generated-by-whois", "someother_field": "more data" }, "summary": "botsrv.btc-goblin.ru"}\n'
             self.assertEqual(mock_stdout.getvalue(), expected)
