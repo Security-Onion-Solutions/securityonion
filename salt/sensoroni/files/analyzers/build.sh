d2bf6d5618faa952da8c083201b74c85459a9805
Jason Ertel <jason.ertel@securityonionsolutions.com>
2022-03-29 14:04:23 -0400

    Add build script to help pre-validate analyzers before pushing

---
+++ salt/sensoroni/files/analyzers/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+HOME_DIR=$(dirname "$0")
+TARGET_DIR=${1:-.}
+
+if ! which pytest &> /dev/null || ! which flake8 &> /dev/null ; then
+	echo "Missing dependencies. Consider running the following command:"
+	echo "  python -m pip install flake8 pytest pytest-cov"
+	exit 1
+fi
+
+flake8 "$TARGET_DIR" --show-source --max-complexity=10 --max-line-length=200 --statistics --doctests
+pytest "$TARGET_DIR" "--cov=$TARGET_DIR" --doctest-modules --cov-report=term --cov-fail-under=90 --cov-config=${HOME_DIR}/pytest.ini
