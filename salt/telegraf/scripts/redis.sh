275aead5b9a07e1a85e7c0def8768c5249c115af
Mike Reeves <mike.reeves@securityonionsolutions.com>
2023-01-04 14:30:28 -0500

    Allow auth for redis check for tgraf

--- salt/telegraf/scripts/redis.sh
+++ salt/telegraf/scripts/redis.sh
@@ -5,12 +5,12 @@
 # https://securityonion.net/license; you may not use this file except in compliance with the
 # Elastic License 2.0.
 {%- set REDIS_PASS = salt['pillar.get']('redis:config:requirepass', '0') %}
-
+export REDISCLI_AUTH={{ REDIS_PASS }}
 # if this script isn't already running
 if [[ ! "`pidof -x $(basename $0) -o %PPID`" ]]; then
 
-    UNPARSED=$(redis-cli -a {{ REDIS_PASS }} llen logstash:unparsed | awk '{print $1}')
-    PARSED=$(redis-cli -a {{ REDIS_PASS }} llen logstash:parsed | awk '{print $1}')
+    UNPARSED=$(redis-cli llen logstash:unparsed | awk '{print $1}')
+    PARSED=$(redis-cli llen logstash:parsed | awk '{print $1}')
 
     echo "redisqueue unparsed=$UNPARSED,parsed=$PARSED"
 
