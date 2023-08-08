a06040c035e6db035426fb001afc1887276cc55a
m0duspwnens <josh.patterson@securityonionsolutions.com>
2023-08-08 09:37:37 -0400

    add WORKERS calculation back to zeekcaptureloss script

--- salt/telegraf/scripts/zeekcaptureloss.sh
+++ salt/telegraf/scripts/zeekcaptureloss.sh
@@ -11,7 +11,11 @@
 if [[ ! "`pidof -x $(basename $0) -o %PPID`" ]]; then
 
     if [ -d "/host/nsm/zeek/spool/logger" ]; then
-      WORKERS={{ salt['pillar.get']('sensor:zeek_lbprocs', salt['pillar.get']('sensor:zeek_pins') | length) }}
+{%- if ZEEKMERGED.config.node.pins %}
+      WORKERS={{ ZEEKMERGED.config.node.pins | length }}
+{%- else %}
+      WORKERS={{ ZEEKMERGED.config.node.lb_procs }}
+{%- endif %}
       ZEEKLOG=/host/nsm/zeek/spool/logger/capture_loss.log
     elif [ -d "/host/nsm/zeek/spool/zeeksa" ]; then
       WORKERS=1
