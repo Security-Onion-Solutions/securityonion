11ca6e3be8018c9b3dd53929f21479e137e113ba
Ryan Hoang <ryanbh1991@gmail.com>
2023-09-04 14:45:53 -0400

    Co-authored-by: HoangLongVu <HoangLongVu@users.noreply.github.com> Co-authored-by: Elijah Gibson <semphorin@users.noreply.github.com> Co-authored-by: Xelial-1064 <Xelial-1064@users.noreply.github.com>

---
+++ salt/sensoroni/files/analyzers/threatfox/threatfox.py
@@ -0,0 +1,25 @@
+import requests
+import argparse
+import json
+
+# API keys (probably unnecessary)
+# da3f0475d11eee92eee0d0a7de0665a6
+# 3cebe3c26ae869e91f094ff99e57428c
+
+def testHashQuery(hash):
+        response = requests.post('https://threatfox-api.abuse.ch/api/v1/', json.dumps({"query":"search_hash", "hash":hash}))
+        #print(response.json())
+        return response.json()
+
+def testIPQuery(ip):
+        response = requests.post('https://threatfox-api.abuse.ch/api/v1/', json.dumps({"query":"search_ioc", "search_term":ip}));
+        print(response.json())
+        return response.json()
+
+#testHashQuery('2151c4b970eff0071948dbbc19066aa4')
+testIPQuery("139.180.203.104")
+
+#Questions
+#Do we need submission for (API KEY)?
+#What data would you like to see from the JSON output?
+
