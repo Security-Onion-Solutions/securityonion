421f64248f79a67dfdc76f83d083944100d574ec
Ryan Hoang <ryanbh1991@gmail.com>
2023-09-15 23:04:51 -0400

    Backbone for Unit Test created

---
+++ salt/sensoroni/files/analyzers/threatfox/threatfox_test.py
@@ -0,0 +1,27 @@
+from io import StringIO
+import sys
+from unittest.mock import patch, MagicMock
+import threatfox
+import unittest
+
+class TestThreatfoxMethods(unittest.TestCase):
+
+        #from Urlhaus_test
+        def test_main_missing_input(self):
+                print(1)
+        def test_main_success(self):
+                print(1)
+        def test_buildReq(self):
+                print(1)
+        def test_sendReq(self):
+                print(1)
+        def test_prepareResults_none(self):
+                print(1)
+        def test_prepareResults_invalidUrl(self):
+                print(1)
+        def test_prepareResults_test(self):
+                print(1)
+        def test_prepareResults_error(self):
+                print(1)
+        def test_analyze(self):
+                print(1)
