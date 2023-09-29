4470a7a9e9784d7a356d630083b4f84789911e97
Ryan Hoang <ryanbh1991@gmail.com>
2023-09-28 21:49:50 -0400

    removed comments

--- salt/sensoroni/files/analyzers/threatfox/threatfox.py
+++ salt/sensoroni/files/analyzers/threatfox/threatfox.py
@@ -7,8 +7,7 @@ import sys
 
 
 def buildReq(observ_type, observ_value):
-    """buildReq takes an input observable type and an input observable value and properly formats them
-     such that we can send a python dictionary object to sendReq."""
+    
 
     if observ_type == 'hash':
         qterms = {'query': 'search_hash', 'hash': observ_value}
@@ -22,9 +21,7 @@ def buildReq(observ_type, observ_value):
 
 
 def sendReq(meta, query):
-    """sendReq takes metadata (the threatfox.yaml file or an optional config file) and 
-    a query (dict object from buildReq containing observable type/value) to request a 
-    report on an observable."""
+   
 
     url = meta['baseUrl']
     response = requests.post(url, json.dumps(query))
@@ -32,8 +29,7 @@ def sendReq(meta, query):
 
 
 def prepareResults(raw):
-    """prepareResults takes json data from sendReq and compiles the response with a
-    summary and status report."""
+    
     if raw != {} and raw['query_status'] == 'ok':
         
         parsed = raw['data'][0]
@@ -52,15 +48,13 @@ def prepareResults(raw):
         else:
             status = 'ok'
 
-    # 'illegl_hash' is not a typo!
+   
     elif raw != {} and raw['query_status'] in ['no_result', 'illegal_search_term', 'illegl_hash']:
-        # not sure if I should set raw to empty here, as leaving it as-is would
-        # give more information other than "no result"
-        # raw = {}
+       
         status = 'info'
         summary = 'no result'
     else:
-        # ask about this in the meeting later
+        
         raw = {}
         status = 'caution'
         summary = 'internal_failure'
@@ -70,40 +64,15 @@ def prepareResults(raw):
 
 
 def analyze(input):
-    """Takes commandline input from main() and checks the appropriate
-    json for supported types and url, then sends formatted data to
-    buildReq -> sendReq -> prepareResults chain."""
+    
     data = json.loads(input)
     meta = helpers.loadMetadata(__file__)
-    
-    # commented, since helpers isn't found properly
-    #helpers.checkSupportedType(meta, data['artifactType'])
+       
     
     query = buildReq(data['artifactType'], data['value'])
     response = sendReq(meta, query)
     return prepareResults(response)
 
-
-# def main():
-#         # gets current directory (for finding yaml file)
-#         dir = os.path.dirname(os.path.realpath(__file__))
-
-#         # create an ArgumentParser object for passing in multiple arguments via command line
-#         parser = argparse.ArgumentParser(description='Query Threatfox for a suspect domain, hash, or IP')
-#         parser.add_argument('artifact', help='JSON with artifact type and value')
-#         parser.add_argument('-c', '--config', metavar='CONFIG_FILE', default=dir + '\\threatfox.yaml',
-#                         help='Parameter for the use of a custom config file in place of the default yaml')
-
-#         # by default, the arg parser stores each argument in a dictionary. parse_args separates
-#         # the arguments into their correspondent parts. they can be accessed as a property of
-#         # a Namespace(?) object.
-#         args = parser.parse_args()
-#         # run if an artifact argument was given
-#         if args.artifact:
-#                 results = analyze(helpers.loadConfig(args.config), args.artifact)
-#                 pprint(results)
-#         else:
-#                 print('ERROR: Input is not in proper JSON format')
 
 def main():
     if len(sys.argv) == 2:
@@ -116,5 +85,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-# to run from shell:
-# py -m threatfox '{\"artifactType\":\"hash\", \"value\":\"2151c4b970eff0071948dbbc19066aa4\"}'
+
