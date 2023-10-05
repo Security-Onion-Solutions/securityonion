699cbd81ddb5b96715b8bbdef5e12d56d86661ab
Ryan Hoang <ryanbh1991@gmail.com>
2023-10-04 23:31:36 -0400

    working implementation

--- salt/sensoroni/files/analyzers/echotrail/echotrail.py
+++ salt/sensoroni/files/analyzers/echotrail/echotrail.py
@@ -6,11 +6,10 @@ import requests
 import argparse
 
 
-def testHash(hashVar):    
-    testHash = hashVar
-    url = "https://api.echotrail.io/" + testHash
-    headers = {"Key": "I7TXsJcq6p2TVwxnsFKcO5rflwLlhjewarRkUPq7I7TXsJcq6p2TVwxnsFKcO5rflwLlhjewarRkUPq7"}
-    response = requests.request('GET', url=url, headers=headers)
+def testHash(hashVar):        
+    url = "https://api.echotrail.io/insights/" + hashVar
+    header = {"x-api-key": "I7TXsJcq6p2TVwxnsFKcO5rflwLlhjewarRkUPq7"}
+    response = requests.request('GET', url=url, headers=header)
     return response.json()
 
 print(testHash("438b6ccd84f4dd32d9684ed7d58fd7d1e5a75fe3f3d12ab6c788e6bb0ffad5e7"))
