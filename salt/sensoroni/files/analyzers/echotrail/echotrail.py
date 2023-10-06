3262c1407c9e6447fd9d13601ff9fe15313d233c
Ryan Hoang <ryanbh1991@gmail.com>
2023-10-05 20:31:41 -0400

    Added some changes

--- salt/sensoroni/files/analyzers/echotrail/echotrail.py
+++ salt/sensoroni/files/analyzers/echotrail/echotrail.py
@@ -4,41 +4,47 @@ import sys
 import requests
 #import helpers
 import argparse
+from pprint import pprint
 
 
-# def testHash(hashVar):        
-#     url = "https://api.echotrail.io/insights/" + hashVar
-#     header = {"x-api-key": "I7TXsJcq6p2TVwxnsFKcO5rflwLlhjewarRkUPq7"}
-#     response = requests.request('GET', url=url, headers=header)
-#     return response.json()
-
-#print(testHash("438b6ccd84f4dd32d9684ed7d58fd7d1e5a75fe3f3d12ab6c788e6bb0ffad5e7"))
-
-def analyze(conf, input):
-    print(1)
-
-def checkConfigRequirements(conf):
-    if "api_key" not in conf:
-        sys.exit(126)
-    else:
-        return True
-    
-def sendReq(conf, meta, observ_value):        
-    url = conf['base_url'] + observ_value
-    #need to account for pro version?
-    headers = {'x-api-key': conf['api_key']}
-    response = requests.request('GET', url=url, headers=headers)
+def testHash(hashVar):        
+    url = "https://api.echotrail.io/insights/" + hashVar
+    header = {"x-api-key": "I7TXsJcq6p2TVwxnsFKcO5rflwLlhjewarRkUPq7"}
+    response = requests.request('GET', url=url, headers=header)   
     return response.json()
 
-def main():
-    dir = os.path.dirname(os.path.realpath(__file__))
-    parser = argparse.ArgumentParser(description='Search Echotrail for a given artifact')
-    parser.add_argument('artifact', help='the artifact represented in JSON format')
-    parser.add_argument('-c','--config', metavar = 'CONFIG_FILE', default=dir + '/echotrail.yaml', help='optional config file to use instead of the default config file')
-    args = parser.parse_args()
-    if args.artifact:
-        results = analyze(helpers.loadConfig(args.config), args.artifact)
-        print(json.dumps(results))
+pprint(testHash("438b6ccd84f4dd32d9684ed7d58fd7d1e5a75fe3f3d12ab6c788e6bb0ffad5e7"))
 
-if __name__ == '__main__':
-    main()
+# def analyze(conf, input):
+#     print(1)
+
+# def checkConfigRequirements(conf):
+#     if "api_key" not in conf:
+#         sys.exit(126)
+#     else:
+#         return True
+    
+# def sendReq(conf, meta, observ_value):        
+#     url = conf['base_url'] + observ_value
+#     #need to account for pro version?
+#     headers = {'x-api-key': conf['api_key']}
+#     response = requests.request('GET', url=url, headers=headers)
+#     return response.json()
+
+# def prepareResults(raw):
+#     summary = raw['filenames'][0][0]
+#     status = 1
+#     results = {'response':raw, 'summary':summary,'status':status}
+
+# def main():
+#     dir = os.path.dirname(os.path.realpath(__file__))
+#     parser = argparse.ArgumentParser(description='Search Echotrail for a given artifact')
+#     parser.add_argument('artifact', help='the artifact represented in JSON format')
+#     parser.add_argument('-c','--config', metavar = 'CONFIG_FILE', default=dir + '/echotrail.yaml', help='optional config file to use instead of the default config file')
+#     args = parser.parse_args()
+#     if args.artifact:
+#         #results = analyze(helpers.loadConfig(args.config), args.artifact)
+#         print(json.dumps(results))
+
+# if __name__ == '__main__':
+#     main()
