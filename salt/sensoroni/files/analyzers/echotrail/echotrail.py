c2da436dfe75c7cf9770170c48950ff528f238ec
Semphorin <mfgotoh@gmail.com>
2023-10-10 15:31:35 -0400

    miniscule adjustments

--- salt/sensoroni/files/analyzers/echotrail/echotrail.py
+++ salt/sensoroni/files/analyzers/echotrail/echotrail.py
@@ -7,10 +7,12 @@ import argparse
 from pprint import pprint
 
 
-# def testHash(hashVar):        
+# def testHash(hashVar):
+#     # alternate response urls. choose one
+#     #url = "https://api.echotrail.io/score/" + hashVar
 #     url = "https://api.echotrail.io/insights/" + hashVar
 #     header = {"x-api-key": "I7TXsJcq6p2TVwxnsFKcO5rflwLlhjewarRkUPq7"}
-#     response = requests.request('GET', url=url, headers=header)   
+#     response = requests.request('GET', url=url, headers=header)
 #     return response.json()
 
 # pprint(testHash("438b6ccd84f4dd32d9684ed7d58fd7d1e5a75fe3f3d12ab6c788e6bb0ffad5e7"))
@@ -19,8 +21,8 @@ def analyze(conf, input):
     checkConfigRequirements(conf)
     meta = helpers.loadMetadata(__file__)
     data = helpers.parseArtifact(input)
-    helpers.checkSupportedType(meta,data['artifactType'])
-    response = sendReq(conf, meta, data['value'])
+    helpers.checkSupportedType(meta, data['artifactType'])
+    response = sendReq(conf, data['value'])
     return prepareResults(response)
 
 def checkConfigRequirements(conf):
@@ -29,17 +31,26 @@ def checkConfigRequirements(conf):
     else:
         return True
     
-def sendReq(conf, meta, observ_value):        
+def sendReq(conf, observ_value):
     url = conf['base_url'] + observ_value
-    #need to account for pro version?
-    headers = {'x-api-key': conf['api_key']}
+    #headers = {'x-api-key': conf['api_key']}
+    headers = {'x-api-key': 'I7TXsJcq6p2TVwxnsFKcO5rflwLlhjewarRkUPq7'}
     response = requests.request('GET', url=url, headers=headers)
     return response.json()
 
 def prepareResults(raw):
-    summary = raw['filenames'][0][0]
-    status = 1
-    results = {'response':raw, 'summary':summary,'status':status}
+    # checking for the 'filenames' key alone does
+    # not work when querying by filename.
+    # So, we can account for a hash query, a filename query,
+    # and anything else with these if statements.
+    if 'filenames' in raw.keys():
+        summary = raw['filenames'][0][0]
+    elif 'tags' in raw.keys():
+        summary = raw['tags'][0][0]
+    else:
+        summary = 'inconclusive'
+    status = 'info'
+    return {'response':raw, 'summary':summary,'status':status}
 
 def main():
     dir = os.path.dirname(os.path.realpath(__file__))
@@ -49,7 +60,8 @@ def main():
     args = parser.parse_args()
     if args.artifact:
         results = analyze(helpers.loadConfig(args.config), args.artifact)
-        print(json.dumps(results))
+        #print(json.dumps(results))
+        pprint(results)
 
 if __name__ == '__main__':
     main()
