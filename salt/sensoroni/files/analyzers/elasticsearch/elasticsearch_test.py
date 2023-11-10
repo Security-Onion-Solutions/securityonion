3765c9995cf24febdfc048c137c2e8bd9cbc196e
Ryan Hoang <ryanbh1991@gmail.com>
2023-11-10 00:30:34 -0500

    added some unit test

--- salt/sensoroni/files/analyzers/elasticsearch/elasticsearch_test.py
+++ salt/sensoroni/files/analyzers/elasticsearch/elasticsearch_test.py
@@ -4,7 +4,7 @@ from unittest.mock import patch, MagicMock
 import unittest
 import elasticsearch
 import helpers
-
+import json
 
 class TestElasticSearchMethods(unittest.TestCase):   
 
@@ -15,6 +15,41 @@ class TestElasticSearchMethods(unittest.TestCase):
             elasticsearch.checkConfigRequirements(conf)
         self.assertEqual(cm.exception.code, 126)
 
+    def test_buildReqHash(self):
+        numberOfResults = 1
+        observableType = "hash"
+
+        expectedQuery = {
+            "from": 0,
+            "size": numberOfResults,
+            "query": {
+                "wildcard": {
+                    observableType: "*"
+                }
+            }
+        }
+        result = elasticsearch.buildReq(observableType,numberOfResults)    
+        self.assertEqual(result, json.dumps(expectedQuery))
+
+    def test_sendReq(self):
+        with patch('requests.post', new=MagicMock(return_value=MagicMock())) as mock:
+            response = elasticsearch.sendReq(
+                'example_index', 'example_query')
+            self.assertIsNotNone(response)
+
+    def test_prepareResults(self):
+        #need to ask Wes how he wants the prepare result output
+        #not done
+        summary = "There are 5 hits recorded."
+        status = 'info'
+        raw = {'_id': "0", "hash": "123"}
+        with patch('requests.post', new=MagicMock(return_value=MagicMock())) as mock:
+            response = elasticsearch.prepareResults(
+                'example_index', 'example_query')
+
+
+        results = elasticsearch.prepareResults(raw)
+        self.assertEqual(1, 1)
     
     
 
