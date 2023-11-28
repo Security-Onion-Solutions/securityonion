6b88e1ba9c7ca73dcae34308c376d828f0aaeae9
Semphorin <mfgotoh@gmail.com>
2023-11-27 20:09:43 -0500

    timeDelta int conversion

--- salt/sensoroni/files/analyzers/elasticsearch/elasticsearch.py
+++ salt/sensoroni/files/analyzers/elasticsearch/elasticsearch.py
@@ -30,7 +30,7 @@ def buildReq(conf, input):
         mappings = dict()
         
     cur_time = datetime.now()
-    start_time = cur_time - timedelta(minutes=conf['timeDeltaMinutes'])
+    start_time = cur_time - timedelta(minutes=int(conf['timeDeltaMinutes']))
 
     if input['artifactType'] in mappings:
         type = mappings[input['artifactType']]
