f32cb1f1153d691394f58295770717b781929ed9
m0duspwnens <josh.patterson@securityonionsolutions.com>
2024-01-30 15:48:10 -0500

    fix find to work with steno and suri pcap

--- salt/telegraf/scripts/oldpcap.sh
+++ salt/telegraf/scripts/oldpcap.sh
@@ -15,7 +15,7 @@ PCAPLOC=/host/nsm/pcap
 if [[ ! "`pidof -x $(basename $0) -o %PPID`" ]]; then
 
     # Get the data
-    OLDPCAP=$(find $PCAPLOC -type f -exec stat -c'%n %Z' {} + | sort | grep -v "\." | head -n 1 | awk {'print $2'})
+    OLDPCAP=$(find $PCAPLOC -type f -exec stat -c'%n %Z' {} + | sort | grep -v "/\." | head -n 1 | awk {'print $2'})
     DATE=$(date +%s)
     AGE=$(($DATE - $OLDPCAP))
 
